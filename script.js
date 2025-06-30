389799c1e0eb76f6a36852a1a6805bbf8c98ddde
mhmdishanul <muhammadihsanularif01@gmail.com>
2025-06-30 09:04:50 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Sembunyikan animasi slide setelah 1.5 detik (waktu sama seperti animasi CSS)
-setTimeout(() => {
-    const slide = document.getElementById("slide-opening");
-    if (slide) {
-      slide.style.display = "none";
-    }
-  }, 1500);
-  
